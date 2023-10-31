1181229c26bff5c2c13a58eed28d331639ce51da
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-10-31 15:49:04 +0100

    add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Restaurant Page',
+      template: './src/index.html',
       filename: 'index.html',
       inject: 'body',
     })
