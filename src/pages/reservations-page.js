a18455d995cbdd97ab5dcc4ef3b9d30850bc4dca
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-11-03 13:23:58 +0100

    add reservations page

--- src/pages/reservations-page.js
+++ src/pages/reservations-page.js
@@ -3,7 +3,6 @@ import VanillaCalendar from '@uvarov.frontend/vanilla-calendar';
 // Basic styles
 import '@uvarov.frontend/vanilla-calendar/build/vanilla-calendar.min.css';
 // Additional styles
-import '@uvarov.frontend/vanilla-calendar/build/themes/light.min.css';
 import '@uvarov.frontend/vanilla-calendar/build/themes/dark.min.css';
 
 
@@ -16,22 +15,41 @@ const reservationsPage = function () {
     contentDiv.appendChild(bodyCardOne);
     
     const cardOneH3 = document.createElement("h3");
-    cardOneH3.textContent = "Place a Reservation";
+    cardOneH3.textContent = "Place a Reservation:";
     bodyCardOne.appendChild(cardOneH3);
 
-    const calendarWrapper = document.createElement("div");
-    bodyCardOne.classList.add("calendar-wrapper");
-    contentDiv.appendChild(calendarWrapper);
 
     const calendarDiv = document.createElement("div");
     calendarDiv.setAttribute("id", "calendar");
-    calendarWrapper.appendChild(calendarDiv);
+    contentDiv.appendChild(calendarDiv);
 
+    const confirmationMessage = (function() {
+        let done = false;
+        return function () {
+            if (!done) {
+                done = true;
+                const bookingConfH2 = document.createElement("h2");
+                bookingConfH2.classList.add("confirmation-message");
+                bookingConfH2.textContent = `Thank you for choosing Wakandine. You will receive an email shortly!`;
+                bookingConfCard.appendChild(bookingConfH2);
+            }
+        }
+    })();
 
-    const calendar = new VanillaCalendar('#calendar');
+    const options = {
+        actions: {
+          clickDay(event, dates) {
+            // dateSelected = dates[0]; >> this can be used to handle the date selected
+            confirmationMessage();
+          },
+        },
+    };
+    const calendar = new VanillaCalendar('#calendar', options);
     calendar.init();
 
-
+    const bookingConfCard = document.createElement("div");
+    bookingConfCard.classList.add("body-card", "card__booking-conf");
+    contentDiv.appendChild(bookingConfCard);
 };
 
 export default reservationsPage;
