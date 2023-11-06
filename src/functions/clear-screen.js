0801fd73bc1d7d91823dd4327c2c00ae65090d5d
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-11-06 10:48:16 +0100

    run build

--- src/functions/clear-screen.js
+++
@@ -1,8 +0,0 @@
-const clearScreen = function(){
-    const content = document.querySelector('.content');
-    while (content.firstChild) {
-        content.removeChild(content.firstChild);
-    }
-};
-
-export default clearScreen;
